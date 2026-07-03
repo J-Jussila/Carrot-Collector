c3c0c686897da5e6ee3b8aa8931473c5474a3e7e
J-Jussila <221941421+J-Jussila@users.noreply.github.com>
2026-07-03 12:25:22 +0300

    final commit
    - added points and win screen
    - finished

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,12 @@ public class Player : MonoBehaviour
     public float moveSpeed;
 
     float hInput, vInput;
+
+    int score = 0;
+
+    public GameObject winText;
+
+    public int winScore;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -25,5 +31,20 @@ public class Player : MonoBehaviour
         vInput = joystick.Vertical * moveSpeed;
 
         transform.Translate(hInput, vInput, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Carrot")
+        {
+            score++;
+
+            Destroy(collision.gameObject);
+
+            if(score >= winScore)
+            {
+                winText.SetActive(true);
+            }
+        }
     }
 }
